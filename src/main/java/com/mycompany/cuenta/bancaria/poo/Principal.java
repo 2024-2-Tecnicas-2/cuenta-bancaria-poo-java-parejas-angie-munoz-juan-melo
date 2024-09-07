c558b1986bb03496594b80047dce508d6f1e6103
jv00000008 <josephd.vargaso@konradlorenz.edu.co>
2024-09-07 14:31:00 -0500

    Solución Cuenta
    Angie Muñoz
    Juan Melo

--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -10,7 +10,7 @@ public class Principal {
         cuenta.setTitular("Lulu");
         System.out.println("Prueba Titular: " + cuenta.getTitular().equals("Lulu"));
         
-        System.out.println("Prueba Numero de Cuenta: " + cuenta.getNumeroCuenta().equals("25685149"));
+        System.out.println("Prueba Numero de Cuenta: " + cuenta.getNumeroCuenta().equals("25683149"));
         
         System.out.println("Prueba Saldo: "+ (cuenta.getSaldo() == 1500.0));
         
@@ -21,20 +21,20 @@ public class Principal {
         System.out.println("Prueba ingresar: " + (cuenta.getSaldo() == 1700.0));
         
         cuenta.retirar(300);
-        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 1400.0));
+        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 1200.0));
         
         cuenta.retirar(2000);
-        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 1400.0));
+        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 1200.0));
         
-        System.out.println("Calcular interes: " + (cuenta.calcularInteres() == 1421.0));
+        System.out.println("Calcular interes: " + (cuenta.calcularInteres() == 1222.5));
         
         cuenta.setTipoInteres(5.0);
-        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1470.0));
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1260.0));
         
         cuenta.setTipoInteres(-2.0);
-        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1470.0));
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1260.0));
         
         cuenta.setTipoInteres(12.0);
-        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1470.0));
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1260.0));
     }
 }
